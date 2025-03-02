2facbb6ddbc2b0c7c0d29631145b41781e0416ef
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-03-02 23:14:19 +0200

    Dependency injection

---
+++ 08_dependency_injection/main.go
@@ -0,0 +1,19 @@
+package dependencyinjection
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "World")
+}
+
+//func main() {
+//log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+//}
